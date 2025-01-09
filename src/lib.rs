0ad0d581984a2cba38adc6a74194f141f6fc73c2
adust09 <shoukitsuda@icloud.com>
2025-01-09 12:11:16 +0900

    add traits

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,11 @@ pub mod field;
 #[macro_use]
 pub mod field;
 #[macro_use]
+mod field_helpers;
 pub mod gf2;
 pub mod gf2p128;
+pub mod gf2psmall;
+pub mod homcom;
+mod traits;
+pub mod veccom;
+pub mod voleith;
